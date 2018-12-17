578f4ae6521e76819ae86bb22c57c8406bfb4094
Diovan <diovanmarques@gmail.com>
2018-12-17 00:43:19 -0200

    Realm Rush Project Started

---
+++ Unity 3d/Realm Rush/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode]              
+public class CubeEditor : MonoBehaviour
+{
+
+    [SerializeField] private float gridSize = 10f;
+
+    private TextMesh label;
+
+    private void Start()
+    {
+        label = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = snapPos;
+
+        label.text = snapPos.x + "," + snapPos.y + "," + snapPos.z;
+    }
+
+}
