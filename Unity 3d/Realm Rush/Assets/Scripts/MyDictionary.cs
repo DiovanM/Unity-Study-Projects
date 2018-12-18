3b9f29a954167b37e65b377fdd6c77fb1c2d664d
Diovan Izy <diovanmarques@gmail.com>
2018-12-18 13:53:04 -0200

    MyDictionary Class

---
+++ Unity 3d/Realm Rush/Assets/Scripts/MyDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+public class MyDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+{
+
+    public override string ToString()
+    {
+        string concat = "";
+        for (int i = 0; i < Count; i++)
+        {
+            concat += "Key: " + this.ElementAt(i).Key + " Value: " + this.ElementAt(i).Value + "\n";
+        }
+
+        return concat;
+    }
+
+}
