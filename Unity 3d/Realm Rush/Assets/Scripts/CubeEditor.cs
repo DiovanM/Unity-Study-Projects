65b36c488c8480d1a999d5cd4ee66ee65a59a2bf
Diovan <diovanmarques@gmail.com>
2018-12-20 00:06:07 -0200

    Modifications to the CubeEditor.cs and added GridBlock.cs and Pathfinder.cs

--- Unity 3d/Realm Rush/Assets/Scripts/CubeEditor.cs
+++ Unity 3d/Realm Rush/Assets/Scripts/CubeEditor.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(GridBlock))]
 [ExecuteInEditMode]              
 public class CubeEditor : MonoBehaviour
 {
 
-    [SerializeField] private float gridSize = 10f;
+    private GridBlock myGridBlock;
 
     private TextMesh label;
+    public Vector3 snapPos;
 
     private void Start()
     {
         label = GetComponentInChildren<TextMesh>();
+        myGridBlock = GetComponent<GridBlock>();
     }
 
     void Update()
     {
-        Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / myGridBlock.GetGridSize()) * myGridBlock.GetGridSize();
+        snapPos.y = 0;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / myGridBlock.GetGridSize()) * myGridBlock.GetGridSize();
 
         transform.position = snapPos;
 
-        label.text = snapPos.x + "," + snapPos.y + "," + snapPos.z;
+        label.text = snapPos.x + "," + snapPos.z;
     }
 
 }
---
+++ Unity 3d/Realm Rush/Assets/Scripts/GridBlock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBlock : MonoBehaviour
+{
+
+    private const int gridSize = 10;
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+}
